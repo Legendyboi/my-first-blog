dde77f68124b382cd4358b9b34ebbc6bcfd941b8
Legendyboi <legendyboi.dev@proton.me>
2024-05-20 12:17:09 +0530

    creating siteConfig

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "LegendyBlog",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "Legendyboi | JollyCoding",
+    links: {
+        twitter: "",
+        github: "https://github.com/Legendyboi",
+    }
+}
+
+export type SiteConfig = typeof siteConfig
