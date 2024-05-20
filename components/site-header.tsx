21cbf8ac050f56f0b6b30e432cc6a256bca50747
Legendyboi <legendyboi.dev@proton.me>
2024-05-20 12:28:52 +0530

    Creating SiteHeader

---
+++ components/site-header.tsx
@@ -0,0 +1,7 @@
+export function SiteHeader() {
+    return (
+        <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-bacgkground/60">
+            Hi
+        </header> 
+    );
+}
