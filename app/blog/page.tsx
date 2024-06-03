d446cafe4be67aa8bd8a8d31c261101168943a1b
Legendyboi <legendyboi.dev@proton.me>
2024-06-03 18:33:58 +0530

    adding moreto blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,8 @@
-
+import { posts } from "#site/content";
 
 export default async function BlogPage(){
-    
+    const displayPosts = posts
+
     return (
      <div className="container max-w-4xl py-6 lg:py-10">
         <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
@@ -13,6 +14,11 @@ export default async function BlogPage(){
             </div>
         </div>
         <hr className="mt-8" />
+        {displayPosts?.length > 0 ? (
+            <ul className="flex flex-col"></ul>
+        ) : (
+            <p>Nothing to see here la</p>
+        )}
     </div>
     );
 }
